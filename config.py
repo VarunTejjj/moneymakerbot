1b122a7e15ceb972d534c3232f12ef1977de80a9
VarunTejjj <varuntejkothapelli.co.in@gmail.com>
2025-07-19 21:02:59 +0530

    Update config.py

--- config.py
+++ config.py
@@ -1,4 +1,11 @@
+# config.py
+
 BOT_TOKEN = "8062425375:AAH_qn7_aPmGyF0nHWhAaf0TDS_jtZFt7uU"
-PRIVATE_CHANNEL_LINK = "https://t.me/yourchannel"
+
+PRIVATE_CHANNEL_LINK = "https://t.me/+nkPAaWA1TI8xOTVl"
+
 UPI_ID = "kothapellivaruntej31@fam"
-KEY_VALIDITY_DAYS = 30  # or any number of days you want keys to be valid
+
+UPI_NAME = "Kotha Pally Sanjana"
+
+KEY_VALIDITY_DAYS = 7  # days access key is valid
