8cb0d0d8414e471f873bbbeabdea052fc90128f3
VarunTejjj <varuntejkothapelli.co.in@gmail.com>
2025-07-19 17:43:30 +0530

    Create config.py

---
+++ config.py
@@ -0,0 +1,6 @@
+# config.py
+BOT_TOKEN = "8062425375:AAH_qn7_aPmGyF0nHWhAaf0TDS_jtZFt7uU"
+PRIVATE_CHANNEL_LINK = "https://t.me/+nkPAaWA1TI8xOTVl"
+UPI_ID = "kothapellivaruntej31@fam"
+UPI_NAME = "Kotha Pally Sanjana"
+KEY_VALIDITY_DAYS = 7
