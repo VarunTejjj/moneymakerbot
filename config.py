bed8c968beb294ebe95365fd464a3e8f20a9cb69
VarunTejjj <varuntejkothapelli.co.in@gmail.com>
2025-07-19 20:47:38 +0530

    Update config.py

--- config.py
+++ config.py
@@ -1,3 +1,4 @@
 BOT_TOKEN = "8062425375:AAH_qn7_aPmGyF0nHWhAaf0TDS_jtZFt7uU"
 PRIVATE_CHANNEL_LINK = "https://t.me/yourchannel"
 UPI_ID = "kothapellivaruntej31@fam"
+KEY_VALIDITY_DAYS = 30  # or any number of days you want keys to be valid
