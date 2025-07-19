25be27d942b22d0391865b4a9c9376d6e734a8a3
VarunTejjj <varuntejkothapelli.co.in@gmail.com>
2025-07-19 18:40:48 +0530

    Update screenshot_checker.py

--- screenshot_checker.py
+++ screenshot_checker.py
@@ -1,12 +1,14 @@
 # screenshot_checker.py
+
 import pytesseract
 from PIL import Image
 from config import UPI_ID, UPI_NAME
 
 def check_screenshot(image_path):
     try:
-        text = pytesseract.image_to_string(Image.open(image_path)).lower().strip()
+        img = Image.open(image_path)
+        text = pytesseract.image_to_string(img).lower().strip()
         return (UPI_ID.lower() in text) and (UPI_NAME.lower() in text)
     except Exception as e:
-        print(f"[ERROR] Unable to process image: {e}")
+        print(f"[ERROR] OCR failed: {e}")
         return False
