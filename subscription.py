858c9e68037ea8e1b9568595dfc3524406c56794
VarunTejjj <varuntejkothapelli.co.in@gmail.com>
2025-07-19 18:05:08 +0530

    Update subscription.py

--- subscription.py
+++ subscription.py
@@ -1,8 +1,9 @@
 # subscription.py
 import random, string, time
+from config import KEY_VALIDITY_DAYS
 
 def generate_key():
     return ''.join(random.choices(string.ascii_uppercase + string.digits, k=10))
 
 def get_expiry():
-    return int(time.time()) + 7 * 24 * 60 * 60
+    return int(time.time()) + KEY_VALIDITY_DAYS * 24 * 60 * 60
